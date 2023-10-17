71b85bcc1247da6faf248209372f1c03fdc1f55b
menyaboo <tosha.kolmay@gmail.com>
2023-10-17 18:31:15 +0700

    del export cpmmit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     HOST_NAME: 'https://menyaboo.github.io/buffspb/',
     //HOST_NAME: 'http://localhost:3000',
   },
-  output: 'export',
+  //output: 'export',
 }
 
 module.exports = nextConfig
