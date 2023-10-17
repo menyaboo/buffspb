bcc52c9babda2e93a8c1b21dfd082a1dc0544d1f
menyaboo <tosha.kolmay@gmail.com>
2023-10-17 16:30:36 +0700

    5n commit

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,13 +1,13 @@
 import {TheHeader} from "@/app/_components/TheNavbar/TheHeader";
 import {TheFooter} from "@/app/_components/TheFooter";
-import {News} from "@/app/_components/News/PostersNews";
+import {PostersNews} from "@/app/_components/News/PostersNews";
 
 export default function Page():JSX.Element {
 
   return (
     <main>
       <TheHeader/>
-      <News/>
+      <PostersNews/>
       <TheFooter/>
     </main>
   )
