0ce597e6479fa368dee7e602db5179d2f6b738e5
menyaboo <tosha.kolmay@gmail.com>
2023-10-19 01:11:49 +0700

    predvaritelnaya versiya commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    HOST_NAME: 'https://buffspb-1aq46coeq-menyaboos-projects.vercel.app/',
+    HOST_NAME: 'https://buffspb-g5ko8999w-menyaboos-projects.vercel.app/',
     //HOST_NAME: 'http://localhost:3000',
   },
 }
